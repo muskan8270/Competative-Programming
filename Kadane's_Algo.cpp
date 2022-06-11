25ff04f7637deaca7ece8e380b02e273469a9462
Muskan Choyla <44960543+muskan8270@users.noreply.github.com>
2022-06-12 00:04:17 +0530

    Kadane's Algorithm

---
+++ Kadane's_Algo.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long long max_so_far = INT_MIN;
+        long long min_ending_here = 0;
+        for(int i=0;i<n;i++){
+            min_ending_here += arr[i];
+        
+        if(max_so_far < min_ending_here){
+             max_so_far = min_ending_here;
+        }
+        if(min_ending_here < 0){
+            min_ending_here = 0;
+        }
+            
+            
+        }
+        return max_so_far;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
